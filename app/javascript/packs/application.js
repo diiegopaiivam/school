578b464a7295b34cd6e04b999abc6ae3b4a5e41c
Diego Paiva <diiegopaiivam@gmail.com>
2020-12-28 16:37:51 -0300

    incluindo js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,12 +5,12 @@
 require("jquery")
 require("@nathanvda/cocoon")
 import Rails from "@rails/ujs"
-//import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 
 
 Rails.start()
-//Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
