35734ed627df20b672b022deb133a2d762e11e0b
Diego Paiva <diiegopaiivam@gmail.com>
2020-12-29 22:22:48 -0300

    set_user

--- app/controllers/profiles__backoffice/perfil_controller.rb
+++ app/controllers/profiles__backoffice/perfil_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesBackoffice::PerfilController < ProfilesBackofficeController
+    before_action :set_user
     def edit
     end
+
+    private 
+        def set_user
+            @user = Profile.find(current_profile.id)
+        end
 end
