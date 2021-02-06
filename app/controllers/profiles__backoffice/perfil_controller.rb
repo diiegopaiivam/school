6f294efc216d68f88200ae142ef53f0f9ad1af94
Diego Paiva <diiegopaiivam@gmail.com>
2021-02-06 18:46:07 -0300

    edit profile

--- app/controllers/profiles__backoffice/perfil_controller.rb
+++ app/controllers/profiles__backoffice/perfil_controller.rb
@@ -1,10 +1,12 @@
 class ProfilesBackoffice::PerfilController < ProfilesBackofficeController
-    before_action :set_user
+    before_action :verify_password, only: [:update]
+    before_action :set_profile
     def edit
     end
 
     def update
-        if @profile.update(user_params)
+        if @profile.update(profile_params)
+            sign_in(@profile, bypass: true) #Fazer o login novamente assim que atualizar o usuário
             redirect_to profiles__backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
           else 
             render :edit
@@ -12,11 +14,18 @@ class ProfilesBackoffice::PerfilController < ProfilesBackofficeController
     end
 
     private 
-        def set_user
-            @user = Profile.find(current_profile.id)
+        def set_profile
+            @profile = Profile.find(current_profile.id)
         end
 
-        def user_params
-            params.require(:profile).permit(:first_name, :last_name, :email)
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+        end
+
+        ## Extraindo password e password confirmation, caso o usuário queira mudar apenas o email
+        def verify_password
+            if params[:profile][:password].blank? && params[:profile][:password_confirmation].blank? 
+                params[:profile].extract!(:password, :password_confirmation)
+            end
         end
 end
