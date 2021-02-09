2e395145eaffb8b405154dc853efb394c29b2028
Diego Paiva <diiegopaiivam@gmail.com>
2021-02-09 17:45:40 -0300

    adicionando radio button no cadastro do perfil

--- app/helpers/profiles_backoffice/perfil_helper.rb
+++ app/helpers/profiles_backoffice/perfil_helper.rb
@@ -1,2 +1,5 @@
 module ProfilesBackoffice::PerfilHelper
+    def gender_select(user, current_gender)
+        user.profile_user.gender == current_gender ? 'btn-primary' : 'btn-default'
+    end
 end
